2d35fc2b095e2a404c2c798134299c3abd5dbae1
novanish <anishchhetri196@gmail.com>
2025-11-24 16:03:03 +0545

    Add configuration files, components, and styles for a new Next.js app

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export default function Home() {
+  return null;
+}
+
+export const metadata: Metadata = {
+  title: "Shop",
+  description: "A shopping app built with Next.js and Tailwind CSS",
+};
