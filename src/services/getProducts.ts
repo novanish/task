58bb47ea2134419e2e6f7fb8a6d231dc95854abd
novanish <anishchhetri196@gmail.com>
2025-11-25 10:19:34 +0545

    feat: add product search, filter, and UI components

--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -4,11 +4,16 @@ interface Params {
   [key: string]: string | undefined | null;
 }
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+if (!BASE_URL) {
+  throw new Error("API base URL is not defined");
+}
+
 export async function getProducts(
   params: Params,
 ): Promise<GetProductsResponse> {
   const query = buildQueryString(params);
-  const url = `https://fakeapi.in/api/products${query ? "?" + query : ""}`;
+  const url = `${BASE_URL}/products${query ? "?" + query : ""}`;
   const res = await fetch(url, {
     cache: "no-store",
   });
