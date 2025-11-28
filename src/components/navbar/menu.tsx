7e32d92f8afeb50ad4fe42a818436fa825b17a2e
novanish <anishchhetri196@gmail.com>
2025-11-28 17:29:44 +0545

    feat(ui): add SheetTitle for NavMenu accessibility

--- src/components/navbar/menu.tsx
+++ src/components/navbar/menu.tsx
@@ -2,7 +2,12 @@
 
 import { NAVIGATION_LINKS } from "@/components/navbar/navigation-links";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
@@ -19,6 +24,7 @@ export function NavMenu() {
         </Button>
       </SheetTrigger>
       <SheetContent side="right" className="w-[80%] sm:w-[400px]">
+        <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
         <nav className="mt-8 flex flex-col gap-4 px-6">
           {NAVIGATION_LINKS.map((item) => (
             <Link
